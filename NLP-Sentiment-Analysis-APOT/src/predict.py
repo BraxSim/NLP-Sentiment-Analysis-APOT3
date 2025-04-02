85ee055cf159ece4e08cf4d3513761551d569c96
Zewen Shen <1465683282@qq.com>
2025-04-02 13:27:38 +1100

    Update predict.py

--- NLP-Sentiment-Analysis-APOT/src/predict.py
+++ NLP-Sentiment-Analysis-APOT/src/predict.py
@@ -1,1 +1,13 @@
 #predict.py
+from transformers import pipeline
+
+
+classifier = pipeline("sentiment-analysis")
+
+def analyze_sentiment(text):
+    result = classifier(text)[0]  # get result of analysis
+    return {"text": text, "sentiment": result['label'], "score": result['score']}
+
+if __name__ == "__main__":
+    text = "I love this product!"
+    print(analyze_sentiment(text))
