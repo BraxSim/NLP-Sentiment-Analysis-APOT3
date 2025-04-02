eddd2b790401f53a04d8aad38d09fec27a8276b3
Zewen Shen <1465683282@qq.com>
2025-04-02 13:28:43 +1100

    Update api.py

--- NLP-Sentiment-Analysis-APOT/src/api.py
+++ NLP-Sentiment-Analysis-APOT/src/api.py
@@ -1,1 +1,14 @@
 #api.py
+from fastapi import FastAPI
+from predict import analyze_sentiment
+
+app = FastAPI()
+
+@app.get("/predict/")
+def predict(text: str):
+    result = analyze_sentiment(text)
+    return result
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
